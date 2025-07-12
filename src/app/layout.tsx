d7ec9cfefd1597ac5f8cb985f4a24c113ab2d534
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2025-07-12 10:08:14 +0545

    Add Header, Footer, and Container to layout
    Imported and integrated Header, Footer, and Container components into the main layout. Also updated body classes to include background and text color for improved styling.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import Container from "@/components/Container";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,9 +28,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`bg-gray-950 text-white ${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Container>
+          <Header />
+          {children}
+          <Footer />
+        </Container>
       </body>
     </html>
   );
