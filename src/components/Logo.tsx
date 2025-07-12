28186cd43ce6e436eb0cf9fee534a742333fb741
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2025-07-12 10:09:00 +0545

    Add Logo component
    Introduces a new Logo component that links to the homepage and displays the logo image using Next.js Image and Link components.

---
+++ src/components/Logo.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+export default function Logo() {
+  return (
+    <Link href="/">
+      <Image src="/logo.png" alt="Logo" width={200} height={50} />
+    </Link>
+  );
+}
