27392e9b28775364dde1feb696b0b789fb8bbc2d
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2025-07-13 14:54:43 +0545

    Fetch and display city events on EventsPage
    EventsPage now fetches events for the selected city from an external API and renders them using the EventsList component. Updated the component to be async and typed the fetched data.

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -1,4 +1,6 @@
+import EventsList from "@/components/EventsList";
 import H1 from "@/components/H1";
+import { EventSphereEvents } from "@/lib/types";
 import React from "react";
 
 type EventsPageProps = {
@@ -7,8 +9,14 @@ type EventsPageProps = {
   };
 };
 
-export default function EventsPage({ params }: EventsPageProps) {
+export default async function EventsPage({ params }: EventsPageProps) {
   const city = params.city;
+
+  const response = await fetch(
+    `https://bytegrad.com/course-assets/projects/evento/api/events?city=${city}`
+  );
+
+  const events: EventSphereEvents[] = await response.json();
 
   return (
     <main className="flex flex-col items-center py-24 px-[20px] min-h-[110vh]">
@@ -21,6 +29,10 @@ export default function EventsPage({ params }: EventsPageProps) {
           </>
         )}
       </H1>
+
+      <div className="mt-8">
+        <EventsList events={events} />
+      </div>
     </main>
   );
 }
